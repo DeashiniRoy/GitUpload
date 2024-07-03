bca39e34b45f732d07663ea0b6fc61d5736bdde7
Deashini Roy <deashini.roy@gmail.com>
2024-07-03 11:30:32 +0530

    Eclipse Commit

--- GitUpload/src/com/src/HelloWorld.java
+++ GitUpload/src/com/src/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
+		System.out.println("Git project modification in VS Code Test");
+		System.out.println("Parctice Git");
 	}
 
 }
